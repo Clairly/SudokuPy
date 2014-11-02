bbecad2129c2c8c72556bbdb9ea9d63ffcf4127d
Victoria Hunsaker <vs184@msstate.edu>
2014-11-02 13:34:40 -0800

    Formatting according to PEP8

--- sudoku.py
+++ sudoku.py
@@ -2,209 +2,217 @@ import itertools
 from sets import Set
 import Queue
 
+
 class Grid(object):
-  def __init__(self, grid):
-    super(Grid, self).__init__()
-    self.grid = grid
+    def __init__(self, grid):
+        super(Grid, self).__init__()
+        self.grid = grid
 
-  def space(self, x, y):
-    if x < 0 or x > 8 or y < 0 or y > 8:
-      return False
-    return self.grid[x][y]
+    def space(self, x, y):
+        if x < 0 or x > 8 or y < 0 or y > 8:
+            return False
+        return self.grid[x][y]
 
-  def line(self, x):
-    if x < 0 or x > 8:
-      return False;
-    return self.grid[x]
+    def line(self, x):
+        if x < 0 or x > 8:
+            return False;
+        return self.grid[x]
 
-  def section(self, x, y):
-    if x < 0 or x > 8 or y < 0 or y > 8:
-      return False
+    def section(self, x, y):
+        if x < 0 or x > 8 or y < 0 or y > 8:
+            return False
 
-    if x in range(0, 3):
-      x_range = range(0, 3);
-    elif x in range(3, 6):
-      x_range = range(3, 6)
-    elif x in range(6, 9):
-      x_range = range(6, 9)
+        if x in range(0, 3):
+            x_range = range(0, 3);
+        elif x in range(3, 6):
+            x_range = range(3, 6)
+        elif x in range(6, 9):
+            x_range = range(6, 9)
 
-    if y in range(0, 3):
-      y_range = range(0, 3)
-    elif y in range(3, 6):
-      y_range = range(3, 6)
-    elif y in range(6, 9):
-      y_range = range(6, 9)
+        if y in range(0, 3):
+            y_range = range(0, 3)
+        elif y in range(3, 6):
+            y_range = range(3, 6)
+        elif y in range(6, 9):
+            y_range = range(6, 9)
 
-    mini_grid = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-    
-    x = 0
-    for i in x_range:
-      y = 0
-      for j in y_range:
-        mini_grid[x][y] = self.grid[i][j]
-        y += 1
-      x += 1
+        mini_grid = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 
-    return mini_grid
+        x = 0
+        for i in x_range:
+            y = 0
+            for j in y_range:
+                mini_grid[x][y] = self.grid[i][j]
+                y += 1
+            x += 1
 
-  def vertical_line(self, y):
-    if y < 0 or y > 8:
-      return False;
-    line = []
-    for x in range(0, 9):
-      line.append(self.grid[x][y])
-    return line 
+        return mini_grid
 
-  def determine_possible_spaces(self, x, y):
-    if self.space(x, y) != 0: return True
+    def vertical_line(self, y):
+        if y < 0 or y > 8:
+            return False;
+        line = []
+        for x in range(0, 9):
+            line.append(self.grid[x][y])
+        return line
 
-    possibles = Set(range(1, 10))
+    def determine_possible_spaces(self, x, y):
+        if self.space(x, y) != 0: return True
 
-    horizontal_set = Set(self.line(x))
-    vertical_set = Set(self.vertical_line(y))
-    section_set = Set(list(itertools.chain(*self.section(x, y))))
+        possibles = Set(range(1, 10))
 
-    possibles = possibles - horizontal_set - vertical_set - section_set
+        horizontal_set = Set(self.line(x))
+        vertical_set = Set(self.vertical_line(y))
+        section_set = Set(list(itertools.chain(*self.section(x, y))))
 
-    possibles = sorted(list(possibles))
+        possibles = possibles - horizontal_set - vertical_set - section_set
 
-    return possibles
+        possibles = sorted(list(possibles))
 
-  def find_all_possibilities(self):
-    # first look for all empty spaces
-    # when one is found, find its possibile entries
-    # and add each entry to the dictionary
-    all_possibles = {}
-    for x in range(0, 9):
-      for y in range(0, 9):
-        if self.space(x, y) == 0:
-          all_possibles[x, y] = self.determine_possible_spaces(x, y)
-    return all_possibles
+        return possibles
 
-  def is_solvable(self):
-    '''returns True if every open space has at least one possible state,
-      returns False if one space is not'''
-    for x in range(0, 9):
-      for y in range(0, 9):
-        if self.space(x, y) == 0 and len(self.determine_possible_spaces(x, y)) == 0: return False
-    return True
+    def find_all_possibilities(self):
+        # first look for all empty spaces
+        # when one is found, find its possible entries
+        # and add each entry to the dictionary
+        all_possibles = {}
+        for x in range(0, 9):
+            for y in range(0, 9):
+                if self.space(x, y) == 0:
+                    all_possibles[x, y] = self.determine_possible_spaces(x, y)
+        return all_possibles
 
-  def is_complete_sequence(self, line):
-    if len(line) != 9: return False
+    def is_solvable(self):
+        '''returns True if every open space has at least one possible state,
+          returns False if one space is not'''
+        for x in range(0, 9):
+            for y in range(0, 9):
+                if self.space(x, y) == 0 and len(self.determine_possible_spaces(x, y)) == 0: return False
+        return True
 
-    # sort list
-    new_line = sorted(line)
+    def is_complete_sequence(self, line):
+        if len(line) != 9: return False
 
-    x = 1
-    for i in new_line:
-      if i != x: return False
-      x = x + 1
-    return True
+        # sort list
+        new_line = sorted(line)
 
-  def is_completed(self):
-    # first verify each space has an answer
-    for x in range(0, 9):
-      for y in range(0, 9):
-        if self.space(x, y) == 0: return False
+        x = 1
+        for i in new_line:
+            if i != x: return False
+            x = x + 1
+        return True
 
-    #verify each vertical line, horizontal line, and section go from 1-9 with no repetition
-    for y in range(0, 9):
-      if self.is_complete_sequence(self.vertical_line(y)) == False: 
-        #print "vertical line ", y, " is incorrect"
-        return False
-    for x in range(0, 9):
-      if self.is_complete_sequence(self.line(x)) == False: 
-        # print "horizontal line ", x, " is incorrect"
-        return False
-    for x in range(0, 3):
-      x = x * 3
-      for y in range(0, 3):
-        y = y * 3
-        flatten_section = list(itertools.chain(*self.section(x, y)))
-        if self.is_complete_sequence(flatten_section) == False: 
-          # print "section ", x, ", ", y, " is incorrect"
-          # print self.section(x, y)
-          return False
+    def is_completed(self):
+        # first verify each space has an answer
+        for x in range(0, 9):
+            for y in range(0, 9):
+                if self.space(x, y) == 0: return False
 
-    return True
+        #verify each vertical line, horizontal line, and section go from 1-9 with no repetition
+        for y in range(0, 9):
+            if self.is_complete_sequence(self.vertical_line(y)) == False:
+            #print "vertical line ", y, " is incorrect"
+                return False
+        for x in range(0, 9):
+            if self.is_complete_sequence(self.line(x)) == False:
+            # print "horizontal line ", x, " is incorrect"
+                return False
+        for x in range(0, 3):
+            x = x * 3
+            for y in range(0, 3):
+                y = y * 3
+                flatten_section = list(itertools.chain(*self.section(x, y)))
+                if self.is_complete_sequence(flatten_section) == False:
+                # print "section ", x, ", ", y, " is incorrect"
+                    # print self.section(x, y)
+                    return False
 
-  def priority(self):
-    num = 0
-    possibles = self.find_all_possibilities()
-    for space in possibles:
-      num = num + len(space)
-    return num
+        return True
 
-  def solve_space(self, x, y, num):
-    if self.space(x, y) == 0: self.grid[x][y] = num
-  
-  def print_line(self, x):
-    print "|", self.space(x,0), self.space(x,1), self.space(x,2), "|", self.space(x,3), self.space(x,4), self.space(x,5), "|", self.space(x,6), self.space(x,7), self.space(x,8), "|"
-    
+    def priority(self):
+        num = 0
+        possibles = self.find_all_possibilities()
+        for space in possibles:
+            num = num + len(space)
+        return num
 
-  def pretty_print(self):
-    print "\n-------------------------"
-    for x in range(0, 9):
-      self.print_line(x)
-      if x == 2 or x == 5:
-        print "-------------------------"
-    print "-------------------------\n"
+    def solve_space(self, x, y, num):
+        if self.space(x, y) == 0: self.grid[x][y] = num
+
+    def print_line(self, x):
+        print "|", self.space(x, 0), self.space(x, 1), self.space(x, 2), "|", self.space(x, 3), self.space(x,
+                                                                                                           4), self.space(
+            x, 5), "|", self.space(x, 6), self.space(x, 7), self.space(x, 8), "|"
+
+
+    def pretty_print(self):
+        print "\n-------------------------"
+        for x in range(0, 9):
+            self.print_line(x)
+            if x == 2 or x == 5:
+                print "-------------------------"
+        print "-------------------------\n"
 
 
 class Puzzle(object):
-  """docstring for Puzzle"""
-  def __init__(self, grid):
-    super(Puzzle, self).__init__()
-    self.grid = Grid(grid)
-    self.possible_states = Queue.PriorityQueue()
-    self.possible_states.put((self.grid.priority(), self.grid))
-    self.set_of_possible_states = Set()
-    self.set_of_possible_states.add(self.grid)
-    self.visited_grids = []
+    """docstring for Puzzle"""
 
-  def solve(self):
-    iterations = 0
-    # only try to solve if it's not already completed and there are still possible grids to examine
-    while self.possible_states.empty() != True:
-      # pop off first item in priority queue and add it to visited grids
-      current_priority, current_grid = self.possible_states.get()
-      self.set_of_possible_states.discard(current_grid)
-      
-      # if the current grid has not already been examined:
-      if current_grid in self.visited_grids: break  
+    def __init__(self, grid):
+        super(Puzzle, self).__init__()
+        self.grid = Grid(grid)
+        self.possible_states = Queue.PriorityQueue()
+        self.possible_states.put((self.grid.priority(), self.grid))
+        self.set_of_possible_states = Set()
+        self.set_of_possible_states.add(self.grid)
+        self.visited_grids = []
 
-      # return current grid if solved successfully 
-      if current_grid.is_completed(): return current_grid
+    def solve(self):
+        iterations = 0
+        # only try to solve if it's not already completed and there are still possible grids to examine
+        while self.possible_states.empty() != True:
+            # pop off first item in priority queue and add it to visited grids
+            current_priority, current_grid = self.possible_states.get()
+            self.set_of_possible_states.discard(current_grid)
 
-      self.visited_grids.append(current_grid)
+            # if the current grid has not already been examined:
+            if current_grid in self.visited_grids: break
 
-      iterations = iterations + 1
-      print "Iteration:", iterations
-      current_grid.pretty_print()
+            # return current grid if solved successfully
+            if current_grid.is_completed():
+                print "Puzzle solved! Yay! Solution:"
+                current_grid.pretty_print()
+                return current_grid
 
-      # get all space options for this grid
-      # if there are no options, this grid is not feasible; move on to the next one
-      options = current_grid.find_all_possibilities()
-      if len(options) == 0: break
-      
-      # create a new grid for each possibility in each open space by:
+            self.visited_grids.append(current_grid)
 
-      # order the options by the lowest number of choices per square
-      for space in sorted(options, key=lambda k: len(options[k]), reverse=True):
+            iterations = iterations + 1
+            print "Iteration:", iterations
+            current_grid.pretty_print()
 
-        x, y = space
-        
-        # create a new grid for each number possible in that space
-        for number_available in current_grid.determine_possible_spaces(x, y):
-          new_grid = Grid(current_grid.grid)
-          new_grid.solve_space(x, y, number_available)
-          
-          # check that the new grid is not in the visited list or in the priority queue or unsolvable
-          if new_grid in self.visited_grids or new_grid.is_solvable == False or new_grid in self.set_of_possible_states:
-            break
-          
-          # add grid to priority queue (priority=total number of possibilities for all open spaces, object=grid)
-          self.possible_states.put((new_grid.priority(), new_grid))
-          self.set_of_possible_states.add(new_grid)
+            # get all space options for this grid
+            # if there are no options, this grid is not feasible; move on to the next one
+            options = current_grid.find_all_possibilities()
+            if len(options) == 0: break
 
-    return False
+            # create a new grid for each possibility in each open space by:
+
+            # order the options by the lowest number of choices per square
+            for space in sorted(options, key=lambda k: len(options[k]), reverse=True):
+                x, y = space
+
+                # create a new grid for each number possible in that space
+                for number_available in current_grid.determine_possible_spaces(x, y):
+                    new_grid = Grid(current_grid.grid)
+                    print "Solving space: (", x, ",", y, ") with", number_available
+                    new_grid.solve_space(x, y, number_available)
+
+                    # check that the new grid is not in the visited list or in the priority queue or unsolvable
+                    if new_grid in self.visited_grids or new_grid.is_solvable == False or new_grid in self.set_of_possible_states:
+                        print "New grid not viable."
+                        break
+
+                    # add grid to priority queue (priority=total number of possibilities for all open spaces, object=grid)
+                    self.possible_states.put((new_grid.priority(), new_grid))
+                    self.set_of_possible_states.add(new_grid)
+
+        return False
